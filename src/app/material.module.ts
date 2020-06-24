0120061cbc63554ba73a563d56d5d487076bb417
greg <gregorywhyte@rogers.com>
2020-06-24 14:39:12 -0400

    backupCopy

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,30 @@
 import { NgModule } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  imports: [MatBadgeModule, MatButtonModule],
-  exports: [MatBadgeModule, MatButtonModule],
+  imports: [
+    MatBadgeModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonToggleModule,
+    MatListModule,
+  ],
+  exports: [
+    MatBadgeModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonToggleModule,
+    MatListModule,
+  ],
 })
 export class MaterialModule {}
