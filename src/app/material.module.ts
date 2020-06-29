d873cd3586cfcc10aa4ad0318e0a46add0d2ed9e
greg <gregorywhyte@rogers.com>
2020-06-29 12:54:06 -0400

    create home component, add side bar and html code  to navbar.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,7 +6,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatListModule } from '@angular/material/list';
-
+import { MatTabsModule } from '@angular/material/tabs';
 @NgModule({
   imports: [
     MatBadgeModule,
@@ -16,6 +16,7 @@ import { MatListModule } from '@angular/material/list';
     MatIconModule,
     MatButtonToggleModule,
     MatListModule,
+    MatTabsModule,
   ],
   exports: [
     MatBadgeModule,
@@ -25,6 +26,7 @@ import { MatListModule } from '@angular/material/list';
     MatIconModule,
     MatButtonToggleModule,
     MatListModule,
+    MatTabsModule,
   ],
 })
 export class MaterialModule {}
